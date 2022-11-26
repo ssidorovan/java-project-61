4eb729815c7b67b177691b5f9f66cccfa52d20ad
Nataliia Sidorova <ssidorova.n@icloud.com>
2022-11-26 15:05:30 +0300

    add new GCD.java

---
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,47 @@
+package hexlet.code.games;
+import hexlet.code.Engine;
+import hexlet.code.Greet;
+import java.util.Scanner;
+
+public class GCD {
+    public static void game() {
+        String userName = Greet.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        int count = 0;
+        while (count < Engine.numRound) {
+            int randomNum1 = Engine.getRandomNum();
+            int randomNum2 = Engine.getRandomNum();
+            Engine.getQuestion(randomNum1 + " "  + randomNum2);
+            String answerUser = scanner.next();
+            String answerCorrect = Integer.toString(test(randomNum1, randomNum2));
+
+            if (answerUser.equals(answerCorrect)) {
+                Engine.correct();
+                count++;
+            } else {
+                Engine.wrong(answerUser, answerCorrect, userName);
+                return;
+            }
+        }
+        Engine.congratulations(userName);
+        scanner.close();
+    }
+    
+    public static int test(int num1, int num2) {
+        if (num1 == 0) {
+            return num2;
+        }
+        else if (num2 == 0) {
+            return num1;
+        }
+        else if (num1 >= num2) {
+            return test(num1 % num2, num2);
+        }
+        else {
+            return test(num1, num2 % num1);
+        }
+    }
+}
