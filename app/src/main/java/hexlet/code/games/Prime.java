da734ded1c3a066eb9d8ee4f1f3a551253f55554
Nataliia Sidorova <ssidorova.n@icloud.com>
2022-11-27 23:28:14 +0300

    add nem Prime.java

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,47 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import hexlet.code.Greet;
+import java.util.Scanner;
+
+public class Prime {
+    public static void game() {
+        String userName = Greet.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        int count = 0;
+        while (count < Engine.numRound) {
+            int randomNum = Engine.getRandomNum(Engine.minRandomNum, Engine.maxRandomNum);
+            String randomNumber = Integer.toString(randomNum);
+            Engine.getQuestion(randomNumber);
+            String answerUser = scanner.next();
+            String answerCorrect = test(randomNum);
+
+            if (answerUser.equals(answerCorrect)) {
+                Engine.correct();
+                count++;
+            } else {
+                Engine.wrong(answerUser, answerCorrect, userName);
+                return;
+            }
+        }
+        Engine.congratulations(userName);
+        scanner.close();
+    }
+
+    public static String test(int number) {
+        if (number < 1) {
+            return "no";
+        } else {
+            int count = 0;
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    count++;
+                }
+            }
+            return count == 2 ? "yes" : "no";
+        }
+    }
+}
