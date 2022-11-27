a681745be6a34ba372dd5c393189231a4368fe41
Nataliia Sidorova <ssidorova.n@icloud.com>
2022-11-27 23:27:29 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,8 +4,7 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
-
-
+import hexlet.code.games.Prime;
 import java.util.Scanner;
 
 public class App {
@@ -18,17 +17,21 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         String choiceUser = scanner.next();
         System.out.println();
+
         switch (choiceUser) {
+            case "0" -> System.out.println("Exit");
             case "1" -> Greet.greeting();
             case "2" -> Even.game();
             case "3" -> Calc.game();
             case "4" -> GCD.game();
             case "5" -> Progression.game();
-            default -> System.out.println(" ");
+            case "6" -> Prime.game();
+            default -> System.out.println("Error, try again");
         }
         scanner.close();
     }
