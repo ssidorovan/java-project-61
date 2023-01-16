e90620528fbcd89532cbd77d3e4ae11dfb71bf7b
Nataliia Sidorova <ssidorova.n@icloud.com>
2023-01-16 19:33:13 +0300

    update GCD.java and Prime.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -30,14 +30,11 @@ public class GCD {
 
 
     public static int gcd(int num1, int num2) {
-        if (num1 == 0) {
-            return num2;
-        } else if (num2 == 0) {
-            return num1;
-        } else if (num1 >= num2) {
-            return gcd(num1 % num2, num2);
-        } else {
-            return gcd(num1, num2 % num1);
+        while (num2 != 0) {
+            int temp = num1 % num2;
+            num1 = num2;
+            num2 = temp;
         }
+        return num1;
     }
 }
--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -29,16 +29,14 @@ public class Prime {
 
 
     public static boolean isPrime(int number) {
-        if (number < 1) {
+        if (number <= 1) {
             return false;
-        } else {
-            int count = 0;
-            for (int i = 1; i <= number; i++) {
-                if (number % i == 0) {
-                    count++;
-                }
-            }
-            return count == 2;
         }
+        for (int i = 2; i <= number / 2; i++) {
+            if ((number % i) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
